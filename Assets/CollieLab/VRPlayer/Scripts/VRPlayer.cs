fea19ae1776720228ee9d84c53df7ae260ac5e9f
David <davidkimighty@gmail.com>
2022-01-10 23:09:34 +0900

    CL-5 Added foundation of VRPlayer

---
+++ Assets/CollieLab/VRPlayer/Scripts/VRPlayer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CollieLab.Controllers
+{
+    public class VRPlayer : MonoBehaviour
+    {
+        #region Serialized Field
+
+        #endregion
+
+
+    }
+}
